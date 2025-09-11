906a8f227e1e89317a25e45d2a1490cfc16cfdc0
yorshka <mail@yors.hk>
2025-09-11 19:29:44 +0900

    fix: delete renderTask class

--- packages/renderer/src/webGPU/core/pipeline/index.ts
+++ packages/renderer/src/webGPU/core/pipeline/index.ts
@@ -4,11 +4,3 @@ export * from './types';
 // Pipeline management classes
 export * from './PipelineFactory';
 export * from './PipelineManager';
-
-// Base render task (existing)
-export * from './BaseRenderTask';
-
-// Existing render tasks
-export * from './coordinate/CoordinateRenderTask';
-export * from './geometry/GeometryRenderTask';
-export * from './scene/SceneRenderTask';
