018d7f0780a57e4333987cd5aed84e5cff5a1740
yorshka <mail@yors.hk>
2025-09-24 01:35:25 +0900

    fix: delete useless module

--- packages/ecs/src/components/rendering/render/index.ts
+++ packages/ecs/src/components/rendering/render/index.ts
@@ -1,5 +1,3 @@
 export * from './RenderComponent';
 export * from './types';
 export * from './WebGPU3DRenderComponent';
-export * from './WebGPUGeometryManager';
-export * from './WebGPUMaterialManager';
